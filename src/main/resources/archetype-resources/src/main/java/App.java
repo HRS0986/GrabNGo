3f254c027eb914ace0b39fbea003e9810d3ccd4e
Hirusha Fernando <heshanhfernando@gmail.com>
2024-10-25 22:21:18 +0530

    Initial Commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+﻿package $com.grabngo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
