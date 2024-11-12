be5e487d77b57f4b349faee8bb37fa0c99290284
Hirusha Fernando <heshanhfernando@gmail.com>
2024-11-12 16:07:37 +0530

    Added Forget Password Feature

--- auth/src/main/java/com/auth/auth/service/AuthService.java
+++ auth/src/main/java/com/auth/auth/service/AuthService.java
@@ -1,18 +1,20 @@
 package com.auth.auth.service;
 
 import com.auth.auth.constants.Messages;
-import com.auth.auth.dto.LoginRequest;
-import com.auth.auth.dto.LoginResponse;
-import com.auth.auth.dto.RefreshTokenRequest;
+import com.auth.auth.dto.*;
+import com.auth.auth.model.VerificationCode;
+import com.auth.auth.repository.VerificationCodeRepository;
 import com.auth.auth.utils.ActionResult;
 import com.auth.auth.model.User;
 import com.auth.auth.repository.AuthRepository;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.ConstraintViolationException;
+import com.auth.auth.utils.VerificationCodeGenerator;
+import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 @Service
 public class AuthService {
@@ -20,11 +22,18 @@ public class AuthService {
     private final AuthRepository authRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final EmailService emailService;
+    private final VerificationCodeRepository verificationCodeRepository;
 
-    public AuthService(AuthRepository authRepository, PasswordEncoder passwordEncoder, JwtService jwtService) {
+    @Value("${webapp.forget-password.url}")
+    private String forgetPasswordUrl;
+
+    public AuthService(AuthRepository authRepository, PasswordEncoder passwordEncoder, JwtService jwtService, EmailService emailService, VerificationCodeRepository verificationCodeRepository) {
         this.authRepository = authRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtService = jwtService;
+        this.emailService = emailService;
+        this.verificationCodeRepository = verificationCodeRepository;
     }
 
     public ActionResult login(LoginRequest credentials) {
@@ -50,6 +59,69 @@ public class AuthService {
             }
         }
         throw new RuntimeException(Messages.TOKEN_INVALID);
+    }
+
+    public ActionResult forgetPassword(String email) throws MessagingException, IOException {
+        var userOptional = authRepository.findByEmailAddress(email);
+        if (userOptional.isEmpty()) {
+            throw new RuntimeException(Messages.USER_NOT_FOUND);
+        }
+
+        String code = VerificationCodeGenerator.generateCode();
+        VerificationCode verificationCode = new VerificationCode();
+        verificationCode.setEmail(email);
+        verificationCode.setVerificationCode(code);
+        verificationCodeRepository.save(verificationCode);
+        emailService.sendForgetPasswordEmail(email, code, forgetPasswordUrl);
+        return new ActionResult(true, Messages.EMAIL_SEND_SUCCESS, null, null);
+    }
+
+    public ActionResult verifyRequest(VerificationRequest verificationRequest) {
+        return verifyRequest(verificationRequest, false);
+    }
+
+    public ActionResult verifyRequest(VerificationRequest verificationRequest, boolean deactivateCode) {
+        var codeOptional = verificationCodeRepository.findByCode(verificationRequest.getVerificationCode());
+        if (codeOptional.isEmpty()) {
+            throw new RuntimeException(Messages.INVALID_VERIFICATION_CODE);
+        }
+
+        VerificationCode verificationCode = codeOptional.get();
+        if (!verificationCode.getEmail().equals(verificationRequest.getEmail())) {
+            throw new RuntimeException(Messages.INVALID_VERIFICATION_CODE);
+        }
+
+        if (!verificationCode.isValid()) {
+            throw new RuntimeException(Messages.INVALID_VERIFICATION_CODE);
+        }
+
+        if (deactivateCode) {
+            verificationCode.setValid(false);
+            verificationCodeRepository.save(verificationCode);
+        }
+
+        return new ActionResult(true, Messages.VERIFICATION_SUCCESS, null, null);
+    }
+
+    public ActionResult resetPassword(ResetPasswordRequest resetPasswordRequest) {
+        var verificationResult = verifyRequest(
+                new VerificationRequest(resetPasswordRequest.getEmail(), resetPasswordRequest.getVerificationCode()),
+                true
+        );
+
+        if (!verificationResult.getStatus()) {
+            return verificationResult;
+        }
+
+        var userOptional = authRepository.findByEmailAddress(resetPasswordRequest.getEmail());
+        if (userOptional.isEmpty()) {
+            throw new RuntimeException(Messages.USER_NOT_FOUND);
+        }
+
+        User user = userOptional.get();
+        user.setPassword(passwordEncoder.encode(resetPasswordRequest.getPassword()));
+        authRepository.save(user);
+        return new ActionResult(true, Messages.PASSWORD_RESET_SUCCESS, null, null);
     }
 
     public ActionResult getUserById(int userId) {
