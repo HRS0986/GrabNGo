80557999456583625423955e484225e3a115468d
SudeeraDilshan <new1818155@gmail.com>
2024-11-21 19:11:54 +0530

    set success responses

--- category/src/main/java/com/category/category/CategoryApplication.java
+++ category/src/main/java/com/category/category/CategoryApplication.java
@@ -1,5 +1,4 @@
 package com.category.category;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
