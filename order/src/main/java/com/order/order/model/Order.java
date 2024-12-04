5891f7ced569c206be03ba41963a57f33f6d77ec
Sewmini  Dias <110555098+SewminiAmanda@users.noreply.github.com>
2024-12-04 19:55:10 +0530

    Update Order.java

--- order/src/main/java/com/order/order/model/Order.java
+++ order/src/main/java/com/order/order/model/Order.java
@@ -19,20 +19,45 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int orderId;
+
     @Column(nullable = false)
     private int userId;
+
     @Column(nullable = false)
     private Double totalPrice;
+
     @Column(nullable = false)
     private String status;
+
     @Column(nullable = false)
     private LocalDateTime createdDateTime = LocalDateTime.now();
+
     @Column(nullable = false)
     private Double discount;
+
+    @Column(nullable = true, length = 50)
+    private String firstname;
+
+    @Column(nullable = true, length = 50)
+    private String lastname;
+
+    @Column(nullable = true, length = 255)
+    private String address;
+
+    @Column(nullable = true, length = 100)
+    private String apartment;
+
+    @Column(nullable = true, length = 50)
+    private String city;
+
+    @Column(nullable = true, length = 50)
+    private String country;
+
+    @Column(nullable = true, length = 20)
+    private String zipcode;
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     @ToString.Exclude
     private List<OrderItem> orderItems;
-
 
 }
